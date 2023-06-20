aca2cef7567a1b15035e3d220245d007dd7d4f0d
subramanian-s-043 <113493600+subramanian-s-043@users.noreply.github.com>
2023-06-20 15:32:56 +0530

    Task - CRUD

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const app = express();
 const connectDB = require('./config/dbConn');
 const userRouter=require('./routes/userRoutes')
+const taskRouter=require('./routes/taskRoutes')
 const PORT=process.env.PORT||8080
 const cors=require('cors')
 const logger=require('morgan')
@@ -12,6 +13,7 @@ app.use(cors())//to handle wrong port number
 app.use(logger('dev'))
 connectDB();
 app.use('/api',userRouter)
+app.use('/task',taskRouter)
 // always last
 mongoose.connection.once("open",()=>{ console.log(`Mongoo is connected 200 ok`); app.listen(PORT,()=> console.log(`Server running ${PORT} `))})
 mongoose.connection.on("error",(err)=>{console.log(err); logEvents(`${err.no}:${err.code}\t${err.syscall}\t${err.hostname}`,'mongoErrLog.log')})
