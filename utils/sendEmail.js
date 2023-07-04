abcbe913ee6fd2892bb85b1304f5a167b2c80e62
Sridhar S <sridharsrinivasan9422@gmail.com>
2023-07-04 16:05:02 +0530

    signin Signup backend updated

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,13 +1,13 @@
-// const nodemailer=require('nodemailer')
+const nodemailer=require('nodemailer')
 
-// const sendEmail = (options)=>{
-//     const transporter = nodemailer.createTransport({
-//         service:process.env.EMAIL_SERVICE,
-//         auth:{
-//             user:process.env.EMAIL_USERNAME,
-//             pass:process.env.EMAIL_PASSWORD
-//         }
-//     })
+const sendEmail = (options)=>{
+    const transporter = nodemailer.createTransport({
+        service:process.env.EMAIL_SERVICE,
+        auth:{
+            user:process.env.EMAIL_USERNAME,
+            pass:process.env.EMAIL_PASSWORD
+        }
+    })
 
     const mailOptions = {
         from: process.env.EMAIL_FROM,
@@ -16,5 +16,5 @@
         html: options.text,
       };
       
-
+    }
 module.exports = sendEmail;
