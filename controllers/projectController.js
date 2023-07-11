984d488ebee4c4aac16ac1927f0a71bf1cab01d9
Sridhar S <sridharsrinivasan9422@gmail.com>
2023-07-11 16:47:00 +0530

    project id updated

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,75 +1,98 @@
-const Projects=require('../models/Projects')
+const Projects = require('../models/Projects')
 const ProjectEmp = require('../models/ProjectEmp')
-const mangoose=require('mongoose')
-const otpgenerator =require('otp-generator')
+const mangoose = require('mongoose')
+const otpgenerator = require('otp-generator')
 
-const getAllProjects=async(req,res)=>{
-    try{
-        const project=await Projects.find({})
-        res.status(200).json({project})
+const getAllProjects = async (req, res) => {
+    try {
+        const project = await Projects.find({})
+        res.status(200).json({ project })
     }
-    catch(error){
-        res.status(404).json({error})
+    catch (error) {
+        res.status(404).json({ error })
     }
 }
 
-const getEmployee=async(req,res)=>{
-    try{
-        const employee=await ProjectEmp.find({})
-        res.status(200).json({employee})
+const getEmployee = async (req, res) => {
+    try {
+        const employee = await ProjectEmp.find({})
+        res.status(200).json({ employee })
     }
-    catch(error){
-        res.status(404).json({error})
+    catch (error) {
+        res.status(404).json({ error })
     }
 }
 
-const createEmployee=async(projectEmpData,res)=>{
-    try{
+const createEmployee = async (projectEmpData, res) => {
+    try {
         const newProjectEmp = new ProjectEmp({
             ...projectEmpData
         })
-        await newProjectEmp.save().then((data)=>{
-            res.status(200).json({data})
-        }).catch((error)=>res.status(404).json({error}))
+        await newProjectEmp.save().then((data) => {
+            res.status(200).json({ data })
+        }).catch((error) => res.status(404).json({ error }))
     }
-    catch(error){
-        res.status(404).json({error})
+    catch (error) {
+        res.status(404).json({ error })
     }
 }
 
-const createProject=async(projectData,res)=>{
-    const OTP=otpgenerator.generate(4,{
-        upperCaseAlphabets:false,
-        specialChars:false,
-        lowerCaseAlphabets:false
+const createProject = async (req, res) => {
+    let projectData = req.body;
+    Task.count().then(async (doc) => {
+        if (doc >= 1) {
+            Projects.findOne({}).sort({ _id: -1 }).exec().then(async function (data) {
+
+                try {
+                    let split = data.projectId.split("-")
+                    const prevProject = parseInt(split[1])
+                    console.log(split)
+                    const currentid = prevProject + 1;
+                    const projectId = "Task-" + currentid
+                    console.log(taskId)
+                    const newProject = new Projects({
+                        ...projectData, projectId
+                    })
+                    await newProject.save().then((data) => {
+                        res.status(200).json({ data })
+                    }).catch((error) => res.status(404).json({ error }))
+                }
+                catch (error) {
+                    res.status(404).json({ error })
+                }
+            })
+        }
+        else {
+            try {
+                const projectId = "Task-1000";
+                const newProject = new Task({
+                    ...projectData,
+                    projectId
+                })
+                await newProject.save().then((data) => {
+                    res.status(200).json(data)
+                }).catch((err) => res.status(404).json({ err }))
+            }
+            catch (err) {
+                res.status(500).json({ err })
+            }
+        }
     })
-    const projectId = 'Project-'+ OTP
-
-    try{
-        const newProject = new Projects({
-            ...projectData,projectId
-        })
-        await newProject.save().then((data)=>{
-            res.status(200).json({data})
-        }).catch((error)=>res.status(404).json({error}))
-    }
-    catch(error){
-        res.status(404).json({error})
-    }
 }
 
-const getProjectById=async(req,res)=>{
-    const project=await Projects.findById(req.params.id).exec()
-    if(!project){
-        res.status(404).json({message:`no project is Available in this id: ${req.params.id}`})
+
+const getProjectById = async (req, res) => {
+    const project = await Projects.findById(req.params.id).exec()
+    if (!project) {
+        res.status(404).json({ message: `no project is Available in this id: ${req.params.id}` })
     }
-    try{
-        await Projects.findOne({_id:req.params.id}).then((data)=>{
-            res.status(200).json({message:"Successfull",data})
-        }).catch((error)=>res.status(404).json({error}))
+    try {
+        await Projects.findOne({ _id: req.params.id }).then((data) => {
+            res.status(200).json({ message: "Successfull", data })
+        }).catch((error) => res.status(404).json({ error }))
     }
-    catch(error){
-        res.status(404).json({error})
+    catch (error) {
+        res.status(404).json({ error })
     }
 }
 
@@ -89,37 +112,37 @@ const getProjectById=async(req,res)=>{
 // }
 const deleteProjectsByIds = async (req, res) => {
     const projectIds = req.body.ids; // Array of project IDs to be deleted
-  
-    try {
-      const deleteResult = await Projects.deleteMany({ _id: { $in: projectIds } });
-  
-      if (deleteResult.deletedCount === 0) {
-        res.status(404).json({ message: `No projects found with the given IDs` });
-      } else {
-        res.status(200).json({ message: `Successfully deleted ${deleteResult.deletedCount} projects` });
-      }
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  };
-  
 
-const updateProjectById=async(req,res)=>{
-    const Project=await Projects.findById(req.params.id).exec()
-    if(!Project){
-        res.status(404).json({message:`no project is Available in this id: ${req.params.id}`})
+    try {
+        const deleteResult = await Projects.deleteMany({ _id: { $in: projectIds } });
+
+        if (deleteResult.deletedCount === 0) {
+            res.status(404).json({ message: `No projects found with the given IDs` });
+        } else {
+            res.status(200).json({ message: `Successfully deleted ${deleteResult.deletedCount} projects` });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
-    try{
-        await Projects.findByIdAndUpdate(req.params.id,{$set:req.body}).then((data)=>{
-            res.status(200).json({message:`Successfully Updated ${req.params.id}`})
-        }).catch((error)=>res.status(404).json({error}))
+};
+
+
+const updateProjectById = async (req, res) => {
+    const Project = await Projects.findById(req.params.id).exec()
+    if (!Project) {
+        res.status(404).json({ message: `no project is Available in this id: ${req.params.id}` })
     }
-    catch(error){
-        res.status(404).json({error})
+    try {
+        await Projects.findByIdAndUpdate(req.params.id, { $set: req.body }).then((data) => {
+            res.status(200).json({ message: `Successfully Updated ${req.params.id}` })
+        }).catch((error) => res.status(404).json({ error }))
+    }
+    catch (error) {
+        res.status(404).json({ error })
     }
 }
 
-module.exports={
+module.exports = {
     getAllProjects,
     createProject,
     getProjectById,
